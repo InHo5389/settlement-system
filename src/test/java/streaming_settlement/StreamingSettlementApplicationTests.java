209e51697447753d6d863bce4147008d6b3e3b87
JEONG <qheogus55@naver.com>
2024-10-21 21:16:01 +0900

    Initial Settings

---
+++ src/test/java/streaming_settlement/StreamingSettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package streaming_settlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingSettlementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
