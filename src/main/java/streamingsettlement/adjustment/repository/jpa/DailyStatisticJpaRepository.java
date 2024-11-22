0932a48bb3d8b7d12eef3beb80993718bf690166
qheog <qheogus55@naver.com>
2024-11-22 22:58:45 +0900

    [#16]feat: 엔티티 수정

--- src/main/java/streamingsettlement/adjustment/repository/jpa/DailyStatisticJpaRepository.java
+++ src/main/java/streamingsettlement/adjustment/repository/jpa/DailyStatisticJpaRepository.java
@@ -1,7 +1,34 @@
 package streamingsettlement.adjustment.repository.jpa;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import streamingsettlement.adjustment.domain.entity.DailyStatistic;
+import streamingsettlement.batch.step.dto.DailyStatisticDto;
 
-public interface DailyStatisticJpaRepository extends JpaRepository<DailyStatistic,Long> {
+import java.time.LocalDateTime;
+
+public interface DailyStatisticJpaRepository extends JpaRepository<DailyStatistic, Long> {
+    Page<DailyStatistic> findByStatisticDateBetween(
+            LocalDateTime startDate,
+            LocalDateTime endDate,
+            Pageable pageable
+    );
+
+    @Query("""
+            SELECT new streamingsettlement.batch.step.dto.DailyStatisticDto(
+                ph.streamingId,
+                COUNT(ph.id),
+                :startDate)
+            FROM PlayHistory ph
+            WHERE ph.createdAt BETWEEN :startDate AND :endDate
+            GROUP BY ph.streamingId
+            """)
+    Page<DailyStatisticDto> countPlayHistoryGroupByStreamingId(
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate,
+            Pageable pageable
+    );
 }
